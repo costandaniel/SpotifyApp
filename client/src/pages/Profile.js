50ffea452e33c24c20e98028967e13f4aadf9ca9
Costan Daniel Solomon <danielcostan99@gmail.com>
2023-03-18 22:49:39 +0200

    finished profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,21 +1,38 @@
 import { useState, useEffect } from "react";
 import { catchErrors } from "../utils";
-import { getCurrentUserProfile, getCurrentUserPlaylists } from "../spotify";
+import {
+  getCurrentUserProfile,
+  getCurrentUserPlaylists,
+  getTopArtists,
+  getTopTracks,
+} from "../spotify";
+import {
+  SectionWrapper,
+  ArtistsGrid,
+  TrackList,
+  PlaylistsGrid,
+} from "../components";
 import { StyledHeader } from "../styles";
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
-  const [playlist, setPlaylists] = useState(null);
+  const [playlists, setPlaylists] = useState(null);
+  const [topArtists, setTopArtists] = useState(null);
+  const [topTracks, setTopTracks] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const UserProfile = await getCurrentUserProfile();
-      setProfile(UserProfile.data);
+      const userProfile = await getCurrentUserProfile();
+      setProfile(userProfile.data);
 
-      const UserPlaylists = await getCurrentUserPlaylists();
-      setPlaylists(UserPlaylists.data);
+      const userPlaylists = await getCurrentUserPlaylists();
+      setPlaylists(userPlaylists.data);
 
-      console.log(UserPlaylists.data);
+      const userTopArtists = await getTopArtists();
+      setTopArtists(userTopArtists.data);
+
+      const userTopTracks = await getTopTracks();
+      setTopTracks(userTopTracks.data);
     };
 
     catchErrors(fetchData());
@@ -38,6 +55,12 @@ const Profile = () => {
                 <div className="header__overline">Profile</div>
                 <h1 className="header__name">{profile.display_name}</h1>
                 <p className="header__meta">
+                  {playlists && (
+                    <span>
+                      {playlists.total} Playlist
+                      {playlists.total !== 1 ? "s" : ""}
+                    </span>
+                  )}
                   <span>
                     {profile.followers.total} Follower
                     {profile.followers.total !== 1 ? "s" : ""}
@@ -46,6 +69,28 @@ const Profile = () => {
               </div>
             </div>
           </StyledHeader>
+
+          {topArtists && topTracks && playlists && (
+            <main>
+              <SectionWrapper
+                title="Top artists this month"
+                seeAllLink="/top-artists"
+              >
+                <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
+              </SectionWrapper>
+
+              <SectionWrapper
+                title="Top tracks this month"
+                seeAllLink="/top-tracks"
+              >
+                <TrackList tracks={topTracks.items.slice(0, 10)} />
+              </SectionWrapper>
+
+              <SectionWrapper title="Playlists" seeAllLink="/playlists">
+                <PlaylistsGrid playlists={playlists.items.slice(0, 10)} />
+              </SectionWrapper>
+            </main>
+          )}
         </>
       )}
     </>
